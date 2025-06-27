26f79935ac241e484f5873ce90216cc0cc648f97
FathanBagas07 <fatan250217@gmail.com>
2025-06-27 14:14:03 +0700

    initial landing page and layouts

---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Landing Page')
+
+@section('head')
+    <link rel="stylesheet" href="{{asset("css/landingPage.css")}}">
+    <link rel="stylesheet" href="{{asset("images">
+@endsection
+
+@section('content')
+    <a href="">Get Started</a>
+@endsection
