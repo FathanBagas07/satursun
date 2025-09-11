1626d973f588c9bea50f57e5f73d530565e3451c
FathanBagas07 <fatan250217@gmail.com>
2025-09-11 23:15:45 +0700

    Refactor: how-it-works blade partial

---
+++ resources/views/components/how-it-works-klien.blade.php
@@ -0,0 +1,55 @@
+{{-- Untuk Klien --}}
+<div class="klien container mb-5">
+    <div class="row align-items-center text-lg-center text-md-start">
+
+        <!-- Text -->
+        <div class="col-lg-8 col-md-6 mb-3 mb-md-0">
+            <h2 class="fw-lighter">
+                CARA KERJA SATURSUN FREELANCE <br> UNTUK <span class="fw-bold">KLIEN</span>
+            </h2>
+        </div>
+
+        <!-- Image -->
+        <div class="col-lg-4 col-md-6 text-center">
+            <img class="img-fluid" width="250" src="{{ asset('images/ilustration-klien-how-it-works.png') }}"
+                alt="Klien How It Works">
+        </div>
+    </div>
+</div>
+
+{{-- Step Klien --}}
+<div class="step-klien row justify-content-center">
+    {{-- Registrasi --}}
+    <div class="col-6 col-md-3 mb-4">
+        <div class="p-3">
+            <i class="bi bi-person-plus fs-2 mb-2"></i>
+            <h5 class="h6">Registrasi</h5>
+            <p class="small text-black">Registrasi sebagai klien untuk mencari jasa yang Anda butuhkan</p>
+        </div>
+    </div>
+    {{-- Posting --}}
+    <div class="col-6 col-md-3 mb-4">
+        <div class="p-3">
+            <i class="bi bi-file-earmark-plus fs-2 mb-2"></i>
+            <h5 class="h6">Posting</h5>
+            <p class="small text-black">Posting pekerjaan dengan tarif sesuai agar dapat diterima pekerja
+            </p>
+        </div>
+    </div>
+    {{-- Tunggu --}}
+    <div class="col-6 col-md-3 mb-4">
+        <div class="p-3">
+            <i class="bi bi-hourglass-split fs-2 mb-2"></i>
+            <h5 class="h6">Tunggu</h5>
+            <p class="small text-black">Tunggu freelancer mengerjakan tugas yang telah Anda berikan</p>
+        </div>
+    </div>
+    {{-- Bayar --}}
+    <div class="col-6 col-md-3 mb-4">
+        <div class="p-3">
+            <i class="bi bi-cash-coin fs-2 mb-2"></i>
+            <h5 class="h6">Bayar</h5>
+            <p class="small text-black">Setelah selesai, bayarkan tarif sesuai dengan yang disepakati</p>
+        </div>
+    </div>
+</div>
