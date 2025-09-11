e706fedaad407fe53377ae8310bd539fb8abc861
FathanBagas07 <fatan250217@gmail.com>
2025-09-11 23:10:25 +0700

    Refactor: add faq blade partial

--- resources/views/pages/landing-page.blade.php
+++ resources/views/pages/landing-page.blade.php
@@ -251,47 +251,11 @@
             </div>
         </section>
 
-        {{-- FAQ --}}
+        {{-- FAQ Section--}}
         <section id="faq" class="faq-section">
             <div class="container">
                 <div class="row g-4">
-                    {{-- FAQ --}}
-                    <div class="col-lg-8">
-                        <h4 class="h5">FAQ Singkat</h4>
-                        <div class="accordion" id="faqAccordion">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="faq1">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
-                                        Bagaimana proses pembayaran?
-                                    </button>
-                                </h2>
-                                <div id="collapse1" class="accordion-collapse collapse" aria-labelledby="faq1"
-                                    data-bs-parent="#faqAccordion">
-                                    <div class="accordion-body">
-                                        Pada versi awal, pembayaran dilakukan di luar platform. Informasi kontak disediakan
-                                        di detail proyek.
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="faq2">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
-                                        Apakah ada rating atau review?
-                                    </button>
-                                </h2>
-                                <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="faq2"
-                                    data-bs-parent="#faqAccordion">
-                                    <div class="accordion-body">
-                                        Fitur rating akan dipertimbangkan di versi berikutnya. Versi MVP menyediakan riwayat
-                                        proyek dan bukti kerja.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @include('components.faq')
                 </div>
             </div>
         </section>
