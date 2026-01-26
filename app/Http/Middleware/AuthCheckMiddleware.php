eaeb2c754f2afa9ec28cd46237a9dc22945c399b
FathanBagas07 <145728133+FathanBagas07@users.noreply.github.com>
2026-01-26 12:18:42 +0700

    Feat: add check auth user login

--- app/Http/Middleware/AuthCheckMiddleware.php
+++ app/Http/Middleware/AuthCheckMiddleware.php
@@ -7,11 +7,38 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthCheckMiddleware
 {
-    public function handle($request, Closure $next, $role)
+    public function handle($request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->role === $role) {
-            return $next($request);
+        // || USER IS LOGGED IN ||
+        if (Auth::check()) {
+            $role = Auth::user()->role;
+
+            if (
+                $request->routeIs(
+                    'auth.sign-in-form',
+                    'auth.sign-up-role',
+                    'auth.sign-up-freelancer',
+                    'auth.sign-up-client'
+                )
+            ) {
+                return match ($role) {
+                    'client' => redirect()->route('client.dashboard'),
+                    'freelancer' => redirect()->route('freelancer.dashboard'),
+                    default => redirect()->route('landing-page'),
+                };
+            }
         }
+
+        // || USER IS NOT LOGGED IN ||
+        if(!Auth::check()) {
+            if ($request->routeIs(
+                'auth.sign-in-form',
+                'auth.sign-up-role',
+            )) {
+                return redirect()->route('auth.sign-in-form');
+            }
+        }
+
         return $next($request);
     }   
 }
